db13ce5c8d139da05a741768985dccf488d6e9d5
Nikita Gulyaev <n.gulyaev@just-ai.com>
2020-05-13 22:03:40 +0300

    gunicorn added, security added

--- server/app.py
+++ server/app.py
@@ -1,11 +1,28 @@
 import subprocess
+import secrets
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
-from fastapi import FastAPI
+app = FastAPI()
+
+security = HTTPBasic()
+
+
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, "admin")
+    correct_password = secrets.compare_digest(credentials.password, "nimda")
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
 
 
 def speedtest_view(app):
     @app.get("/speedtest")
-    def speedtest():
+    def speedtest(_=Depends(get_current_username)):
         res = subprocess.check_output(['speedtest', '--format=json']).decode()
         return res
 
@@ -14,7 +31,7 @@ def speedtest_view(app):
 
 def health_check_view(app):
     @app.get("/healthCheck")
-    def health_check():
+    def health_check(_=Depends(get_current_username)):
         return "Ok"
 
     return health_check
